a50543a2eb91b186be7e3d0d75fd77d7f3a92533
Tim Leung <tim.fh.leung@gmail.com>
2025-09-29 19:54:22 -0400

    added letterboxing

--- data.py
+++ data.py
@@ -7,9 +7,9 @@ import numpy as np
 import skimage.io as io
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import pylab
 
-import itertools
+#%%
+# {1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag', 32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard', 37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove', 41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle', 46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
 
 #%%
 # You can format your JSON document using Shift+Alt+F or Format Document from the context menu.
@@ -24,9 +24,8 @@ ANCHOR_BOXES = torch.tensor([
     [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
 ])
 
-#%%
 class data_COCO():
-    def __init__(self, datadir, datatype, anchors, num_anchors_per_scale = 3, scale_size = [13, 26, 52], iou_thresh = 0.5):
+    def __init__(self, datadir, datatype, anchors, num_anchors_per_scale = 3, scale_size = [13, 26, 52], iou_thresh = 0.5, target_size = [416,416]):
         self.datadir = datadir
         self.datatype = datatype
 
@@ -35,20 +34,32 @@ class data_COCO():
         self.catIds = self.coco.getCatIds(self.cats)
         self.imgIds = self.coco.getImgIds(catIds = self.catIds)
         self.labels = {cat['id'] : cat['name'] for cat in self.cats}
+        self.target_size = target_size
 
         self.anchors = anchors
         self.num_anchors_per_scale = num_anchors_per_scale
         self.scale_size = scale_size
         self.iou_thresh = iou_thresh
 
+        self.coco_to_yolo_dict = {cat['id']: i for i, cat in enumerate(self.cats)}
+
     def __len__(self):
         return len(self.imgIds)
 
-    @staticmethod
-    def coco_to_yolo(anns, img):
-        labels = torch.tensor([ann['category_id'] for ann in anns], dtype = torch.long)
+    def coco_to_yolo(self, anns, img):
+        # labels = torch.tensor([ann['category_id'] for ann in anns], dtype = torch.long)
+        labels = torch.tensor([self.coco_to_yolo_dict[ann['category_id']] for ann in anns], dtype = torch.long)
         bbox = torch.tensor([ann['bbox'] for ann in anns], dtype = torch.float32)
 
+        #some images may not have any annotations, check and discard
+        if bbox.numel() == 0:
+            print(f'bbox.size() = {bbox.size()}')
+            print(f'labels.size() = {labels.size()}')
+            print('No annotations')
+            return None, None
+
+        # coco bbox format is xmin, ymin, w, h, where the +x is to the right and +y is down
+        # we convert to yolo bbox format, xcenter, ycenter, w, h
         bbox[:, 0] = bbox[:, 0] + 0.5*bbox[:, 2]
         bbox[:, 1] = bbox[:, 1] + 0.5*bbox[:, 3]
         bbox[:, [0,2]] /= img['width']
@@ -67,18 +78,53 @@ class data_COCO():
         bbox[:, [1,3]] *= img['height']
 
         return labels, bbox
+    
+    @staticmethod
+    def letterbox(img, target_size = [640,640]):
+        #img format is h, w, c
+        import cv2
+        h, w = img.shape[:2]
+        scale = min(target_size[0]/w, target_size[1]/h)
+        # print(f'w={w}, h={h}, scale={scale}')
 
-    def __getitem__(self, idx):
+        top, bottom = (target_size[1]-int(scale*h))//2, target_size[1] - (target_size[1]-int(scale*h))//2 - int(scale*h)
+        left, right = (target_size[0]-int(scale*w))//2, target_size[0] - (target_size[0]-int(scale*w))//2 - int(scale*w)
+
+        img_resized = cv2.resize(img, (int(w*scale), int(h*scale)), interpolation=cv2.INTER_LINEAR)
+        img_resized = cv2.copyMakeBorder(img_resized, top, bottom, left, right, cv2.BORDER_CONSTANT, None, value = 0)
+        # print(f'top={top}, side={left}')
+        # return torch.tensor(img_resized.reshape(3, target_size[0], target_size[1])), (scale, scale), (left, top)
+        # return torch.tensor(img_resized), scale, (left, top)
+        return torch.tensor(img_resized), scale, (left, bottom)
+
+    def rescaled_bbox(self, bbox, scale, padding, img):
+        # print(f'padding = {padding}')
+        bbox = bbox.clone()
+        bbox *= scale
+        bbox[:, 0] += padding[0]/img['width']
+        bbox[:, 1] += padding[1]/img['height']
+        # print(f'padding[1]={padding[1]}, img[height]={img['height']}, padding[1]//img[height]={padding[1]//img['height']}')
+        return bbox
+
+    def __getitem__(self, idx, img_resize = True):
+        # print(f'idx = {idx}')
         img = self.coco.loadImgs(self.imgIds[idx])[0]
-        I = io.imread(f'{self.datadir}/images/{self.datatype}/{img['file_name']}')
-
         annIds = self.coco.getAnnIds(imgIds=img['id'])
         anns = self.coco.loadAnns(annIds)
-
+        # output = [torch.zeros((self.num_anchors_per_scale, S, S, 6), dtype=torch.long) for S in self.scale_size]
         output = [torch.zeros((self.num_anchors_per_scale, S, S, 6)) for S in self.scale_size]
-        labels, bbox = self.coco_to_yolo(anns, img)
 
-        ious = self.compute_iou_wh(bbox[:,2:4], self.anchors)
+        if img_resize == True:
+            I, scale, padding = self.letterbox(io.imread(f'{self.datadir}/images/{self.datatype}/{img['file_name']}'), self.target_size)
+        else:
+            I, scale, padding = torch.tensor(io.imread(f'{self.datadir}/images/{self.datatype}/{img['file_name']}')), 1., (0, 0)
+
+        labels, bbox_normalized = self.coco_to_yolo(anns, img)
+        if labels is None:
+            return I.detach().clone(), tuple(output)
+
+        bbox_normalized = self.rescaled_bbox(bbox_normalized, scale, padding, img)
+        ious = self.compute_iou_wh(bbox_normalized[:,2:4], self.anchors)
 
         # Loop through the ious for all bounding boxes.
         # Determine the best anchor box size based on iou (for each scale)
@@ -91,29 +137,35 @@ class data_COCO():
             #Looping over scales
             for idx, anchor_idx in enumerate(anchor_iou.view(-1)):
                 scale_idx = idx // self.num_anchors_per_scale
-                if idx % self.num_anchors_per_scale == 0:
+                if idx % self.num_anchors_per_scale == 0: #reset anchor_set = False for each new scale
                     anchor_set = False
 
-                i, j = self.scale_size[scale_idx] * bbox[bbox_idx, 0], self.scale_size[scale_idx] * bbox[bbox_idx, 1]  # which cell
+                i, j = self.scale_size[scale_idx] * bbox_normalized[bbox_idx, 0], self.scale_size[scale_idx] * bbox_normalized[bbox_idx, 1]  # which cell
+                # print(f'idx={idx}, i={i}, j={j}')
 
                 if output[scale_idx][anchor_idx, int(i), int(j), 0] != 1 and anchor_set == False:
-                    x_cell, y_cell = i - int(i), j - int(j)  # both between [0,1]
+                    x_cell, y_cell = i - int(i), j - int(j)  # return fraction parts of i and j
                     width_cell, height_cell = (
-                        bbox[bbox_idx, 2] * self.scale_size[scale_idx],
-                        bbox[bbox_idx, 3] * self.scale_size[scale_idx],
+                        bbox_normalized[bbox_idx, 2] * self.scale_size[scale_idx],
+                        bbox_normalized[bbox_idx, 3] * self.scale_size[scale_idx],
                     )  # can be greater than 1 since it's relative to cell
                     abox = torch.tensor([x_cell, y_cell, width_cell, height_cell])
 
-                    output[scale_idx][anchor_idx, int(i), int(j), 0] = 1
+                    # output[scale_idx][anchor_idx, int(i), int(j), 0] = 1
+                    output[scale_idx][anchor_idx, int(i), int(j), 0] = int(1)
                     output[scale_idx][anchor_idx, int(i), int(j), 1:5] = abox
                     output[scale_idx][anchor_idx, int(i), int(j), 5] = labels[bbox_idx]
+
+                    # print(f'idx={idx}, abox={abox}')
+                    # print(f'output[scale_idx][anchor_idx, int(i), int(j), 1:5]={output[scale_idx][anchor_idx, int(i), int(j), 1:5]}')
+                    # print(f'labels[bbox_idx]={labels[bbox_idx]}')
 
                     anchor_set = True
                 elif anchor_set == True and iou[scale_idx][anchor_idx] > self.iou_thresh:
                     output[scale_idx][anchor_idx, int(i), int(j), 0] = -1
  
         #image needs to be of type float32 and label needs to be torch long (the label is an index, not name)
-        return I, tuple(output)
+        return I.detach().clone(), tuple(output)
         # return I, torch.tensor(output, dtype=torch.long)
     
     # calculate iou between abox and bbox where iou is defined as: area of overlap / area of union
@@ -169,14 +221,69 @@ class data_COCO():
 if __name__ == "__main__":
     data = data_COCO(datadir = './coco/', datatype = 'val2017', anchors = ANCHOR_BOXES)
     print(data.__len__())
+
     #%%
-    img, output = data.__getitem__(16)
-    img, output = data.__getitem__(22)
+    dataloader = torch.utils.data.DataLoader(data, batch_size = 1, shuffle = True)
+    train_features, train_labels = next(iter(dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Feature batch shape: {train_features[0].size()}")
+    # print(f"Feature batch shape: {train_features[0].squeeze().size()}")
+    print(f"Labels batch shape: {train_labels[0].size()}")
+    print(f"Labels batch output: {train_labels[0][0,:,:,:,:].size()}")
+    print(f"Labels batch output: {train_labels[1][0,:,:,:,:].size()}")
+    print(f"Labels batch output: {train_labels[2][0,:,:,:,:].size()}")
+    print(f"Labels batch output: {train_labels[2][0,0,0,0,:]}")
+    # print(f"Labels batch output: {train_labels[2][0][0][0][0]}")
+
+
+    img = train_features[0]
+    # img = train_features[0].squeeze()
+    label = train_labels[0]
+    # plt.imshow(img, cmap="gray")
+    plt.imshow(img)
+    plt.show()
+
+    #%%
+    print(dataloader.device)
+
+    #%%
+    no_annot = []
+    for i, data in enumerate(dataloader):
+        # print(data[0].device)
+        print(f'data[0].size() = {data[0].size()}')
+        print(f'data[1][0].size() = {data[1][0].size()}')
+        print(f'data[1][1].size() = {data[1][1].size()}')
+        print(f'data[1][2].size() = {data[1][2].size()}')
+
+        data[1][0] = data[1][0].to('cuda')
+        data[1][1] = data[1][1].to('cuda')
+        data[1][2] = data[1][2].to('cuda')
+        # print(data[0].device)
+        train_features, train_labels = data[0].to('cuda'), data[1]
+        print(i)
+    #%%
+    test = torch.tensor([])
+    print(test.size())
+
+    if test.numel() == 0:
+        print('no elements')
+
+
+    #%%
+    # img, output = data.__getitem__(16, img_resize=True) #w = 640, h = 480
+    img, output = data.__getitem__(22, img_resize=True) #height = 427, width = 640
+    # img, output = data.__getitem__(1680)
+    # img, output = data.__getitem__(160)
+    # img, output = data.__getitem__(54) #height = 427, width = 640
     # plt.imshow(img)
 
+    # print(f'img.size() = {img.size()}')
+
+
     # Create figure and axes
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(1, figsize=(30,30))
     ax.imshow(img)
+    # trainerval.plot_imgs(8, font_size = 3, axis_size = 2, shuffle = True, dpi = 224*8)
 
     print(output[0].size())
     print(output[0].size()[1])
@@ -184,21 +291,21 @@ if __name__ == "__main__":
     print(output[1].size())
     print(output[2].size())
 
-    #bounding boxes are defined as x_topleft, y_topleft, w, h - where x, y are the top left corner of the image (for the coco format)
+    #bounding boxes output is xmin, ymin, w, h - where x, y are the top left corner of the image (for the coco format)
     #YOLO format, x_center, y_center, w, h
     def convert_ij_to_xy(bbox, i, j, width, height, scale_size = 13):
-        x = (bbox[0] + i) * width / scale_size
-        y = (bbox[1] + j) * height / scale_size
+        xmin = (bbox[0] + i) * width / scale_size
+        ymin = (bbox[1] + j) * height / scale_size
         w = bbox[2] * width / scale_size
         h = bbox[3] * height / scale_size
 
-        return torch.tensor([x, y, w, h], dtype=torch.float32)
+        return torch.tensor([xmin, ymin, w, h], dtype=torch.float32)
 
     def plot_boxes(coord, fig_name):
         x = coord[0] - 0.5*coord[2]
         y = coord[1] - 0.5*coord[3]
 
-        rect = patches.Rectangle(xy = (x, y), width = coord[2], height = coord[3], linewidth=1, linestyle = '--', edgecolor='r', facecolor='none')
+        rect = patches.Rectangle(xy = (x, y), width = coord[2], height = coord[3], linewidth=3, linestyle = '--', edgecolor='r', facecolor='none')
         fig_name.add_patch(rect)
 
     # print(output[0][0])
@@ -213,7 +320,8 @@ if __name__ == "__main__":
                         # bbox0 = convert_ij_to_xy(output[k,i,j,1:5], i, j, 640, 480)
                         # bbox0 = convert_ij_to_xy(output[k,i,j,1:5], i, j, 399, 640)
                         bbox0 = convert_ij_to_xy(output[k,i,j,1:5], i, j, 640, 427)
-                        print(f'i={i}, j={j}, anchor_idx={k}, label={data.labels[int(output[k,i,j,5])]}, bbox={output[k,i,j,1:5]}, bbox0={bbox0}')
+                        print(f'i={i}, j={j}, anchor_idx={k}, bbox={output[k,i,j,1:5]}, bbox0={bbox0}')
+                        # print(f'i={i}, j={j}, anchor_idx={k}, label={data.labels[int(output[k,i,j,5])]}, bbox={output[k,i,j,1:5]}, bbox0={bbox0}')
 
                         plot_boxes(bbox0, ax)
                         count += 1
